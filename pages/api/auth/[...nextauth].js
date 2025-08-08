363cf4013346d5bc560651f79f760dc1bd1acc82
Madhu Varsha <madhuvarsha0608@gmail.com>
2025-08-08 09:08:09 +0530

    nextauth modified

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,7 +12,7 @@ if (!process.env.GOOGLE_CLIENT_SECRET) {
   console.error('GOOGLE_CLIENT_SECRET is not set')
 }
 
-export default NextAuth({
+const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -53,39 +53,10 @@ export default NextAuth({
   },
   pages: {
     signIn: '/',
-    error: '/auth/error',
+    // error: '/auth/error',
   },
   debug: process.env.NODE_ENV === 'development',
 })
 
-// import NextAuth from 'next-auth'
-// import GoogleProvider from 'next-auth/providers/google'
-
-// export default NextAuth({
-  // providers: [
-  //   GoogleProvider({
-  //     clientId: process.env.GOOGLE_CLIENT_ID,
-  //     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  //     authorization: {
-  //       params: {
-  //         prompt: 'select_account',
-  //       },
-  //     },
-  //   }),
-  // ],
-//   session: {
-//     strategy: 'jwt', 
-//   },
-//   jwt: {
-//     encryption: true
-//   },
-//   secret: process.env.NEXTAUTH_SECRET,
-//   callbacks: {
-//     async redirect({ url, baseUrl }) {
-//       if (url.startsWith("/")) return `${baseUrl}${url}`
-//       else if (new URL(url).origin === baseUrl) return url
-//       return baseUrl
-//     },
-//   },
-//   debug: process.env.NODE_ENV === 'development',
-// })
+export { handler as default }
+export { handler as GET, handler as POST }
